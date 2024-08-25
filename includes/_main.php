6e4973e75324573406c8f6aa8be4c904a47053e8
YoussefGhollamallah <youssef.gh@live.fr>
2024-08-25 14:38:20 +0200

    commit initial

---
+++ includes/_main.php
@@ -0,0 +1,8 @@
+<main>
+    <?php include_once "_about.php"; ?>
+    <?php include_once "_competence.php"; ?>
+    <?php include_once "_projets.php"; ?>
+    <?php include_once "_formation.php"; ?>
+    <?php include_once "_contact.php"; ?>
+
+</main>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Youssef Ghollamallah - Portfolio</title>
+</head>
+<body>
+
+    <?php include_once "includes/_header.php"; ?>
+
+    <?php include_once "includes/_main.php"; ?>
+    
+    <?php include_once "includes/_footer.php"; ?>
+
+</body>
+</html>
