e6d2ec4147a8cf4dd383d63c0d3cb2f422261ee1
YoussefGhollamallah <youssef.gh@live.fr>
2024-08-25 18:48:03 +0200

    mise en place de la section about

---
+++ includes/_about.php
@@ -0,0 +1,11 @@
+<section id="about">
+    <article>
+        <h2>À propos de moi</h2>
+        <p>Je suis un développeur passionné par les nouvelles technologies et le design interactif. Voici quelques-unes de mes réalisations.</p>
+        <p>Après plusieurs années d'expérience dans la restauration en tant que pâtissier et cuisinier, j'ai développé des compétences solides en travail d'équipe, en rigueur et en écoute.
+        J'ai une passion de longue date pour le domaine du numérique, j'ai profité de la période Covid pour oser une reconversion professionnelle vers le développement web.</p>
+        <p>J'ai suivi une formation intensive en Bootcamp, suivie d'un Bachelor IT à La Plateforme, là où j'ai découvert l'univers des métiers du numérique
+        <p>Actuellement, je poursuis cette aventure en alternance en tant que au sein de La Plateforme.</p>           
+    </article>
+    <img id="photo" src="assets/pictures/photo.png" alt="photo de profil">
+</section>
