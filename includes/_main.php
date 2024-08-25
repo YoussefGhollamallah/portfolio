a576b6bb6008a259407d8ba062b404132e8623f3
YoussefGhollamallah <youssef.gh@live.fr>
2024-08-25 18:49:19 +0200

    mise à jour et fin des imports des fichiers includes

--- includes/_main.php
+++ includes/_main.php
@@ -1,6 +1,6 @@
 <main>
     <?php include_once "_about.php"; ?>
-    <?php include_once "_competence.php"; ?>
+    <?php include_once "_competences.php"; ?>
     <?php include_once "_projets.php"; ?>
     <?php include_once "_formation.php"; ?>
     <?php include_once "_contact.php"; ?>
